9ece6f6b7f24c6f05b591d532594d0e7dda7d772
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2021-05-26 19:23:58 -0300

    Agrega formtypes para las formas de "pc-build".

---
+++ client/src/form_types.ts
@@ -0,0 +1,67 @@
+export interface IMotherboardForm {
+  id?: string;
+  cpu_socket: string;
+  cpu_brand: string;
+  ram_version: string;
+  min_frec: string;
+  max_frec: string;
+  video_socket: string;
+  model_size: string;
+  watts: string;
+}
+
+export interface IProductForm {
+  id?: string;
+  name: string;
+  description: string;
+  image: string;
+  price: string;
+  stock: string;
+  category: string;
+  brand: string;
+}
+
+export interface ICabinetForm {
+  id?: string;
+  size: string;
+  generic_pws: string;
+}
+
+export interface IRamForm {
+  id?: string;
+  ram_version: string;
+  memory: string;
+  min_frec: string;
+  max_frec: string;
+  watts: string;
+}
+
+export interface IDiskStorageForm {
+  id?: string;
+  total_storage: string;
+  type: string;
+  mbs: string;
+  watts: string;
+}
+
+export interface IPowerSupplyForm {
+  id?: string;
+  watts: string;
+  certification: string;
+}
+
+export interface IProcessorForm {
+  id?: string;
+  cores: string;
+  frecuency: string;
+  socket: string;
+  watts: string;
+}
+
+export interface IVideoCardForm {
+  id?: string;
+  version: string;
+  memory: string;
+  clock_speed: string;
+  watts: string;
+}
