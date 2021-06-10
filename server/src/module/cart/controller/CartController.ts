f593324fa9b870739c784972ef94a070c19bd143
Roman Mettler <Roman.Mettler@gmail.com>
2021-06-10 05:42:27 -0300

    Crea rutas en CartController

--- server/src/module/cart/controller/CartController.ts
+++ server/src/module/cart/controller/CartController.ts
@@ -1,4 +1,5 @@
-import { Application } from "express";
+import { Application, NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import { injectable } from "inversify";
 import { AbstractController } from "../../abstractClasses/abstractController";
 
@@ -10,7 +11,52 @@ export class CartController extends AbstractController {
         this.ROUTE_BASE = "/cart"
     }
 
-    configureRoutes(app: Application) {
+    configureRoutes(app: Application): void {
         const ROUTE = this.ROUTE_BASE
+        app.get(`/api${ROUTE}`, this.getAllCarts.bind(this))
+        app.get(`/api${ROUTE}/:id`, this.getSingleCart.bind(this))
+        app.post(`/api${ROUTE}/:cartId/item`, this.addCartItem.bind(this))
+        app.delete(`/api${ROUTE}/:cartId/item/:itemId`, this.removeCartItem.bind(this))
+        app.put(`/api${ROUTE}/:cartId/item/:itemId`, this.modifyCartItemQuantity.bind(this))
+    }
+
+    async getAllCarts(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            res.status(StatusCodes.NOT_IMPLEMENTED).send(StatusCodes.NOT_IMPLEMENTED)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async getSingleCart(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            res.status(StatusCodes.NOT_IMPLEMENTED).send(StatusCodes.NOT_IMPLEMENTED)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async addCartItem(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            res.status(StatusCodes.NOT_IMPLEMENTED).send(StatusCodes.NOT_IMPLEMENTED)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async removeCartItem(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            res.status(StatusCodes.NOT_IMPLEMENTED).send(StatusCodes.NOT_IMPLEMENTED)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async modifyCartItemQuantity(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            res.status(StatusCodes.NOT_IMPLEMENTED).send(StatusCodes.NOT_IMPLEMENTED)
+        } catch (err) {
+            next(err)
+        }
     }
 }
---
+++ server/src/module/cart/module.ts
@@ -0,0 +1,1 @@
+export { CartController } from './controller/CartController'
