613a9e9706924c16a961a4817c1d08d1ca975302
Roman Mettler <Roman.Mettler@gmail.com>
2021-06-25 03:48:14 -0300

    fix error de validacion al crear producto que inicie con numeros

--- server/src/module/product/helper/edit_dto_validator.ts
+++ server/src/module/product/helper/edit_dto_validator.ts
@@ -5,12 +5,12 @@ export const validateEditProductDto = Joi.object({
         .required(),
     name: Joi.string()
         .min(3)
-        .max(20),
+        .max(40),
     brand: Joi.string(),
     image: Joi.string(),
     description: Joi.string()
         .min(3)
-        .max(20),
+        .max(100),
     price: Joi.number(),
     stock: Joi.boolean(),
     id_category: Joi.number()
