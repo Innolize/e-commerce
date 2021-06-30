e46c3d2c893b0761a3d1603f61276d6b131865c6
Roman Mettler <Roman.Mettler@gmail.com>
2021-06-30 07:28:50 -0300

    Implementa creacion de ordenes en repository del modulo de ordenes

--- server/src/module/order/interfaces/IOrderCreate.ts
+++ server/src/module/order/interfaces/IOrderCreate.ts
@@ -1,3 +1,8 @@
 import { IOrder } from "./IOrder";
+import { IOrderItemCreate } from "./IOrderItemCreate";
 
-export type IOrderCreate = Omit<IOrder, 'id' | 'cartItems' | 'payment' >
+export type IOrderItemAssociated = Omit<IOrderItemCreate, 'order_id'>
+
+export type IOrderCreate = Omit<IOrder, 'id' | 'cartItems' | 'payment'> & {
+    orderItems?: IOrderItemAssociated[]
+}
--- server/src/module/order/repository/OrderRepository.ts
+++ server/src/module/order/repository/OrderRepository.ts
@@ -1,10 +1,40 @@
 import { inject, injectable } from "inversify";
+import { TYPES } from "../../../config/inversify.types";
 import { AbstractRepository } from "../../abstractClasses/abstractRepository";
+import { Cart } from "../../cart/entities/Cart";
+import { Order } from "../entities/Order";
+import { OrderItemModel } from "../model/OrderItemModel";
+import { OrderModel } from "../model/OrderModel";
 
 @injectable()
 export class OrderRepository extends AbstractRepository {
     constructor(
+        @inject(TYPES.Order.OrderModel) private orderModel: typeof OrderModel,
+        @inject(TYPES.Order.OrderModel) private orderItemModel: typeof OrderItemModel
     ) {
         super()
+    }
+
+    async create(cart: Cart, userId: number): Promise<Order> {
+        if (!cart.cartItems?.length) {
+            throw new Error("Impossible to create an order, no cart items in current cart")
+        }
+        const itemsArray = cart.cartItems.map(item => {
+            const { product_id, quantity, product } = item
+            if (!product) {
+                throw new Error('Product not populated')
+            }
+            const orderItem = {
+                price_per_unit: item.product!.price,
+                product_id: product_id,
+                quantity: item.quantity,
+                total: item.product!.price * quantity
+            }
+            return orderItem
+        })
+        const currentOrder = await this.orderModel.create({ user_id: userId, payment_id: 1, orderItems: itemsArray }, { include: { association: OrderModel.associations.cartItems } })
+        console.log(currentOrder)
+        return currentOrder
+
     }
 }
