1b76d139596aa5f3f22e14bed82a78ceaf50dfd1
Roman Mettler <Roman.Mettler@gmail.com>
2021-07-26 13:43:50 -0300

    Crea nuevos tests para productService

--- server/src/module/product/service/productService.ts
+++ server/src/module/product/service/productService.ts
@@ -2,9 +2,7 @@ import { inject, injectable } from "inversify";
 import { TYPES } from "../../../config/inversify.types";
 import { AbstractService } from "../../abstractClasses/abstractService";
 import { Brand } from "../../brand/entity/Brand";
-import { BrandService } from "../../brand/module";
 import { Category } from "../../category/entity/Category";
-import { CategoryService } from "../../category/module";
 import { Product } from "../entity/Product";
 import { IProductCreate } from "../interfaces/IProductCreate";
 import { IProductEdit } from "../interfaces/IProductEdit";
@@ -12,21 +10,20 @@ import { GetProductsDto } from "../dto/getProductsDto";
 import { GetProductsReqDto } from "../dto/getProductsReqDto";
 import { IProductRepository } from "../interfaces/IProductRepository";
 import { IProductService } from "../interfaces/IProductService";
+import { IBrandRepository } from "../../brand/interfaces/IBrandRepository";
+import { ICategoryRepository } from "../../category/interfaces/ICategoryRepository";
 
 @injectable()
-export class ProductService extends AbstractService implements IProductService{
-    private productRepository: IProductRepository
-    private brandService: BrandService
-    private categoryService: CategoryService
+export class ProductService extends AbstractService implements IProductService {
     constructor(
-        @inject(TYPES.Product.Repository) productRepository: IProductRepository,
-        @inject(TYPES.Brand.Service) brandService: BrandService,
-        @inject(TYPES.Category.Service) categoryService: CategoryService
+        @inject(TYPES.Product.Repository) private productRepository: IProductRepository,
+        @inject(TYPES.Brand.Repository) private brandRepository: IBrandRepository,
+        @inject(TYPES.Category.Service) private categoryRepository: ICategoryRepository
     ) {
         super()
         this.productRepository = productRepository
-        this.brandService = brandService
-        this.categoryService = categoryService
+        this.brandRepository = brandRepository
+        this.categoryRepository = categoryRepository
     }
     async deleteProduct(id: number): Promise<boolean> {
         return await this.productRepository.deleteProduct(id)
@@ -43,12 +40,12 @@ export class ProductService extends AbstractService implements IProductService{
     async createProduct(product: IProductCreate): Promise<Product> {
         return await this.productRepository.createProduct(product)
     }
-    async findProductById(id: number): Promise<Error | Product> {
+    async findProductById(id: number): Promise<Product> {
         return await this.productRepository.getById(id)
     }
     async verifyCategoryAndBrandExistence(categoryId: number, brandId: number): Promise<{ category: Category, brand: Brand }> {
-        const category = await this.categoryService.findCategoryById(categoryId)
-        const brand = await this.brandService.findBrandById(brandId)
+        const category = await this.categoryRepository.findCategoryById(categoryId)
+        const brand = await this.brandRepository.getById(brandId)
         return { category, brand }
     }
 }
