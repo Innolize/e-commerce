e54f511738fe687be94bcbcb6e7a62df28385a81
daxas-boop <matias.daniel.zabalegui@gmail.com>
2021-08-10 21:11:20 -0300

    Desplaza hasta la parte superior en cambio de ruta

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ProtectedAdminRoute from "src/routes/ProtectedAdminRoute";
 import AdminRoutes from "./AdminRoutes";
 import DefaultRoutes from "./DefaultRoutes";
+import ScrollToTop from "./ScrollToTop";
 
 const Routes = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <ProtectedAdminRoute path="/admin" component={AdminRoutes} />
         <Route path="/" component={DefaultRoutes} />
