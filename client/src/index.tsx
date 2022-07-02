e10dff17ccee4d6b0ebed55d1da7b69e356dff07
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-07-02 20:33:05 -0300

    Revert "Add Analitics API for Vercel"

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,4 @@
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { sendToVercelAnalytics } from "./vitals";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-reportWebVitals(sendToVercelAnalytics);
